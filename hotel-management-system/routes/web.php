45bde1263965975cd0738e0ff5191db0a7cbeab0
Mahmoudawd4 <mahmoudawd54@gmail.com>
2022-02-24 21:33:56 +0200

    manager CRUD

--- hotel-management-system/routes/web.php
+++ hotel-management-system/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ManagerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -7,22 +8,24 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register web routes for your applimanagerion. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/dashboard', function () {
-    return view('dashboard.layout.master');
-});
-Route::get('/dashboard/managers', function () {
-    return view('dashboard.manager.index');
-});
-Route::get('/dashboard/managers/create', function () {
-    return view('dashboard.manager.create');
-});
+//crad manageregory
 
+//index managere
+Route::get('/manager', [ManagerController::class, 'index'])->name('manager.index');
+//show managere
+// Route::get('manager/{id}',[ManagerController::class, 'show'])->name('manager.show');
+//crete
+Route::get('manager/create',[ManagerController::class, 'create'])->name('manager.create');
+Route::post('manager/store',[ManagerController::class, 'store'])->name('manager.store');
+//update
+Route::get('manager/edit/{id}',[ManagerController::class, 'edit'])->name('manager.edit');
+Route::post('manager/update/{id}',[ManagerController::class, 'update'])->name('manager.update');
+
+//Delete
+Route::delete('manager/delete/{id}',[ManagerController::class, 'destroy'])->name('manager.delete');
